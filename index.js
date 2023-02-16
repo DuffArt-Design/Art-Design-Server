783131c3fcc76ecba4b8f3949b7e9725b289472d
Stevenrej <steveo732@gmail.com>
2023-02-16 16:43:46 -0600

    deleted unused code

--- index.js
+++ index.js
@@ -29,54 +29,9 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.once('open', function() {
   console.log('Connected to MongoDB database!');
-  // Write your code here.
 });
-
-
-
-// HOW TO UPLOAD //////////////////////////////////////////////////////////////////////////
-
-// const res = cloudinary.uploader.upload('https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg', {public_id: 'olympic_flag'});
-// res.then((data) => {
-//   console.log(data);
-//   console.log(data.secure_url);
-// }).catch((err) => {
-//   console.log(err);
-// });
-// // Generate 
-// const url = cloudinary.url('olympic_flag', {
-//   width: 100,
-//   height: 150,
-//   Crop: 'fill',
-// });
-// console.log(url);
-
-// HOW TO GRAB ALL ///////////////////////////////////////////////////////////
 
 app.use(pictureRouter);
-
-app.get('/photos', (req, res) => {
-  cloudinary.api.resources({ max_results: 100 })
-    .then(result => {
-      console.log(`Received photos`);
-      console.log(result);
-      res.json(result);
-    })
-    .catch(err => {
-      console.error(`Error fetching photos`);
-      console.error(err);
-      res.status(500).json({ error: err.message });
-    });
-});
-
-
-
-app.get('/folder', (req, res) => {
-  cloudinary.api.root_folders()
-    .then(result => res.json(result))
-    .catch(err => res.status(500).json({ error: err.message }));
-});
-
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
